5d9bb9fa99b3e5280521479b6db512fc263019e1
Crisnzx <cristovao.neto@academico.ifpb.edu.br>
2021-04-05 13:53:40 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>depois do almoço: 1 - criar um repositório no github, 2 - emular o android no Android Studio</Text>
+      <Image style={{width:200, height:200}} source={{uri: 'https://github.com/Crisnzx.png'}}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightgray',
     alignItems: 'center',
     justifyContent: 'center',
   },
